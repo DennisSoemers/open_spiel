e23dfe59f2597d88fc38bb535b74dae9a96b66eb
Dennis Soemers <d.soemers@gmail.com>
2020-07-22 15:43:04 +0200

    fixed some ludii build things

--- open_spiel/games/ludii_game.cc
+++ open_spiel/games/ludii_game.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "open_spiel/games/ludii/ludii_game.h"
+#include "open_spiel/games/ludii_game.h"
 
 #include <algorithm>
 #include <memory>
